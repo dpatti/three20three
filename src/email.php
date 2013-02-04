4d7d297eb778d1231e7a6955d61dee6e4b4fed35
Doug Patti <douglas.patti@gmail.com>
2013-02-04 02:56:02 -0500

    Fix email and htaccess

--- src/email.php
+++ src/email.php
@@ -8,12 +8,13 @@ $clean_fields = array();
 
 foreach ($text_fields as $f) {
   // Not much to do here so long as we are sending the email as text and not html.
-  $clean_fields = $_POST[$f];
+  // But magic quotes are on so that's cool
+  $clean_fields[$f] = stripslashes($_POST[$f]);
 }
 
 $budget = sprintf("$%d - $%d", $_POST['budget'][0], $_POST['budget'][1]);
 
-$to = str_replace(file_get_contents('email_config'), "\n", ",");
+$to = str_replace("\n", ",", file_get_contents('email_config'));
 $from = "${clean_fields['name']} <${clean_fields['email']}>";
 $subject = "Contact via 323productions.com";
 
@@ -23,14 +24,20 @@ $message = "This was submitted via the website contact form.\n\n"
          . "Budget: ${budget}\n"
          . "Project: ${clean_fields['project']}\n";
 
-$headers = "From: $from\r\n"
-         . "Reply-To: $from\r\n"
-         . "Content-type: text/plain; charset=\"UTF-8\"\r\n";
+$headers = "From: Three20Three Website <contact@323productions.com>\r\n"
+         . "Reply-To: <contact@323productions.com>\r\n"
+         . "Return-Path: <contact@323productions.com>\r\n"
+         . "Content-Type: text/plain; charset=UTF-8\r\n"
+         . "MIME-Version: 1.0\n";
 
-if (!mail($to, $subject, $message, $headers)) {
+$params  = "-fcontact@323productions.com";
+
+if (!mail($to, $subject, $message, $headers, $params)) {
   header("HTTP/1.1 500 Internal Server Error");
   // Try to mail me about it
-  mail("admin@323productions.com", "Contact form failure", date('r'));
+  $error = date('r') . "\n---\n$to\n---\n$subject\n---\n$message\n---\n$headers";
+  mail("admin@323productions.com", "Contact form failure", "", $headers);
+  echo "Mail error";
   exit;
 }
 
